06e4161d3d7ef1b726e830e2e3130a4ef3202364
LAB-MM <LAB-MM@LAB-MM-PC14>
2025-02-27 11:37:25 +0700

    tugas pbo 26 feb

---
+++ src/triwulan_2401093014/relasidemo.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package triwulan_2401093014;
+
+/**
+ *
+ * @author LAB-MM
+ */
+public class relasidemo {
+    public static void main(String[] args){
+        //a few numbers
+        int i = 37;
+        int j = 42;
+        int k = 42;
+        System.out.println("Variable values...");
+        System.out.println("i = "  +i);
+        System.out.println("j = "+j);
+        System.out.println("k = "+k);
+        
+        //lebih besar dari
+        System.out.println("Greater than...");
+        System.out.println("i > j = "  +(i > j)); //false
+        System.out.println("j > i = "  +(j > i)); //true
+        System.out.println("k > j = "  +(k > j)); //false
+        
+        //lebih besar atau sama dengan
+        System.out.println("Greater than or equal to...");
+        System.out.println("i >= j = "  +(i >= j)); //false
+        System.out.println("j >= i = "  +(j >= i)); //true
+        System.out.println("k >= j = "  +(k >= j)); //false
+        
+        //sama dengan
+        System.out.println("Equal to...");
+        System.out.println("i == j = "  +(i == j)); //false
+        System.out.println("k == j = "  +(k == j)); //false
+        
+        //tidak sama dengan
+        System.out.println("Not equal to...");
+        System.out.println("i != j = "  +(i != j)); //false
+        System.out.println("k != j = "  +(k != j)); //false
+        
+        
+        
+        
+        
+    }
+    
+}
